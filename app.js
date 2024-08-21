e649c82fbdbb944a902b7f2bc6c8e1ab9e9aa07b
Travis Crabbe <travcrabbe@gmail.com>
2024-08-21 16:26:54 -0700

    Finished game functionality Added CSS classes to make buttons glow depending     on game outcome.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreboard_div = document.querySelector(".scoreboard");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
@@ -21,16 +21,52 @@ function newRound(userChoice) {
         case "rockscissors":
         case "paperrock":
         case "scissorspaper":
-            console.log("User wins!");
+            win(userChoice, computerChoice);
             break;
         case "rockpaper":
         case "paperscissors":
         case "scissorsrock": 
-            console.log("CPU wins!");
+            lose(userChoice, computerChoice);
             break;
         default: 
-            console.log("It's a draw!")
+            draw(userChoice);
     }
+}
+
+function win(userChoice, computerChoice) {
+    const userChoice_classList = document.getElementById(userChoice).classList;
+    console.log("User wins!");
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    // Make letter uppercase
+    const upperUserChoice = userChoice.charAt(0).toUpperCase() + userChoice.substring(1);
+
+    result_p.innerHTML = `${upperUserChoice} beats ${computerChoice}. You win!`;
+    userChoice_classList.add("green-glow");
+    setTimeout(() => userChoice_classList.remove("green-glow"), 300);
+
+}
+
+function lose(userChoice, computerChoice) {
+    const userChoice_classList = document.getElementById(userChoice).classList;
+    console.log("User loses!");
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    // Make letter uppercase
+    const upperComputerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.substring(1);
+
+    result_p.innerHTML = `${upperComputerChoice} beats ${userChoice}. You lose!`;
+    userChoice_classList.add("red-glow");
+    setTimeout(() => userChoice_classList.remove("red-glow"), 300);
+}
+
+function draw(userChoice) {
+    const userChoice_classList = document.getElementById(userChoice).classList;
+    console.log("Draw!");
+    const upperUserChoice = userChoice.charAt(0).toUpperCase() + userChoice.substring(1);
+    result_p.innerHTML = `You both threw ${userChoice}. Draw!`;
+    userChoice_classList.add("gray-glow");
+    setTimeout(function() {userChoice_classList.remove("gray-glow")}, 300);
 }
 
 rock_div.addEventListener("click", function() {
